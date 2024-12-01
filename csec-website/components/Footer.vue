f11864d00934adaacfa16edde833137305e9e83a
betim-hodza <bhodza02@gmail.com>
2024-11-30 20:52:55 -0600

    FEAT: added basic footer info UPDATE: made home card span the whole page

---
+++ csec-website/components/Footer.vue
@@ -0,0 +1,52 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    setup() {
+        const currentYear = new Date().getFullYear();
+        return {
+            currentYear,
+        };
+    },
+});
+
+</script>
+
+<template>
+    <div class="flex space-x-3 ">
+        <!-- its the official CSEC -->
+        <p>CSEC &copy; {{ currentYear }} </p>
+        <!-- Ppl can email us thru here -->
+        <p>
+            Contact: csecclub@proton.me
+        </p>
+
+        <!-- social links -->
+         <!-- discord -->
+        <UButton 
+        icon="ic:baseline-discord" 
+        to="https://discord.gg/gPTZJVCFX8"
+        size="sm"
+        :ui="{ rounded: 'rounded-full' }"
+        >
+        </UButton>
+        <!-- instagram -->
+        <UButton 
+        icon="mdi:instagram" 
+        to="https://www.instagram.com/uta.csec/"
+        size="sm"
+        :ui="{ rounded: 'rounded-full' }"
+        >
+        </UButton>
+        <!-- Mavorgs -->
+        <UButton 
+        icon="basil:university-outline" 
+        to="https://mavorgs.campuslabs.com/engage/organization/csec"
+        size="sm"
+        :ui="{ rounded: 'rounded-full' }"
+        >
+        </UButton>
+
+        
+    </div>
+</template>
