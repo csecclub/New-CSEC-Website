3e5ccb5a1f3dd3548d2baa4252472e7aa8216a08
bay <bhodza02@gmail.com>
2024-12-03 18:59:44 -0500

    UPDATE: how does one move cat to right whilst keeping words to left?

--- csec-website/components/Home.vue
+++ csec-website/components/Home.vue
@@ -1,26 +1,30 @@
 
 <template>
-    <h1>CSEC</h1>
-    <h3>UTA's Ethical CyberSecurity Club</h3>
-
-    <img src="~/assets/michi.png" />
-
-    <p>Stay up to date by joinging our discord server!</p>
+    <div class="flex">
+        <div class="flex-col m-4">
+            <h1 class="text-3xl">CSEC</h1>
+            <h3 class="text-xl">UTA's Ethical CyberSecurity Club</h3>
+        </div>
+        <img class="flex self-end" src="~/assets/michi.png" />
+    </div>
     
-    <!-- Discord Link button -->
-    <!-- https://ui.nuxt.com/components/button -->
-    <UButton 
-        icon="ic:baseline-discord" 
-        to="https://discord.gg/gPTZJVCFX8"
-        size="lg"
-        >
-        Join our Discord
-    </UButton>
-
-
-    <h3> Check out our other websites! </h3>
-    <ul>
-        <li><NuxtLink to="https://csecnote.utacsec.com">CSEC-Note</NuxtLink></li>
-        <li><NuxtLink to="https://battleground-uta.vercel.app/">CTF-Playground</NuxtLink></li>
-    </ul>
+    
+        <p>Stay up to date by joinging our discord server!</p>
+        
+        <!-- Discord Link button -->
+        <!-- https://ui.nuxt.com/components/button -->
+        <UButton 
+            icon="ic:baseline-discord" 
+            to="https://discord.gg/gPTZJVCFX8"
+            size="lg"
+            >
+            Join our Discord
+        </UButton>
+    
+    
+        <h3> Check out our other websites! </h3>
+        <ul>
+            <li><NuxtLink to="https://csecnote.utacsec.com">CSEC-Note</NuxtLink></li>
+            <li><NuxtLink to="https://battleground-uta.vercel.app/">CTF-Playground</NuxtLink></li>
+        </ul>
 </template>
