5818ff982bc1251aededf9c387ea2b5131c9909b
Betim H <106614572+Betim-Hodza@users.noreply.github.com>
2024-12-03 09:10:46 -0600

    UPDATE: added description for each past event

--- csec-website/pages/events.vue
+++ csec-website/pages/events.vue
@@ -3,7 +3,7 @@
         <!-- NavBar acts as a header -->
         <Navbar></Navbar>
 
-        <h1>CSEC's Past Events</h1>
+        <h1 class="">CSEC's Past Events</h1>
 
         <PastCarasol></PastCarasol>
 
