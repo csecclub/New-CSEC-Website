37b2c9ffd95b6bf96078c0776b0ea3b6036110f7
max-eboh <142966396+max-eboh@users.noreply.github.com>
2025-11-14 15:36:08 -0600

    Revert "added DomainChangeBanner component to notify users of website relocation"
    This reverts commit d743377bf38588eba044cd361d6dd8ed30183621.

--- csec-website/app.vue
+++ csec-website/app.vue
@@ -3,9 +3,6 @@
     <UCard class="w-screen h-screen flex flex-col justify-between overflow-hidden-y overflow-scroll">
         <Navbar />
 
-        <DomainChangeBanner />
-
-
         <NuxtPage class=""/>
         <template #footer>
             <Footer />
